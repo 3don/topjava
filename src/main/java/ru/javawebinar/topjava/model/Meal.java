942a00b487d6b2fa4326983edd99572e342f3d7a
Евгений <litvinov.ne@gmail.com>
2022-04-30 17:56:52 +0300

    HW04 optional hw solved fix

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -13,7 +13,7 @@ import java.time.LocalTime;
         @NamedQuery(name = Meal.GET_MEAL, query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
         @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
         @NamedQuery(name = Meal.FILTERED, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id " +
-                "AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC"),
+                "AND m.dateTime >= :startDate AND m.dateTime < :endDate ORDER BY m.dateTime DESC"),
         @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id ORDER BY m.dateTime DESC")
 })
 
